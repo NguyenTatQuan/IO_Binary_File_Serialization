eb92041b4fab15f81ea2327afbb24b8e76db3994
kikon123 <tatquan2001@gmail.com>
2024-03-27 16:54:47 +0700

    bai tap

--- src/controller/Manager.java
+++ src/controller/Manager.java
@@ -2,7 +2,21 @@ package controller;
 
 import model.Product;
 
-public class Manager {
+import java.io.File;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Manager implements Serializable {
+    private List<Product> products;
+
+    public Manager() {
+        products = new ArrayList<>();
+    }
+
+    public static void readwrite(List<Product> products){
+        File file = new File("li");
+
+    }
 
 }
