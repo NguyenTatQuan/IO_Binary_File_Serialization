f671d637bb5a36a8587e807e3c5df1a125be7e69
kikon123 <tatquan2001@gmail.com>
2024-03-27 17:01:52 +0700

    baitap

--- src/controller/Manager.java
+++ src/controller/Manager.java
@@ -2,8 +2,7 @@ package controller;
 
 import model.Product;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +13,21 @@ public class Manager implements Serializable {
         products = new ArrayList<>();
     }
 
-    public static void readwrite(List<Product> products){
-        File file = new File("li");
+    public static void readwrite(List<Product> products) {
+        File file = new File("listproduct");
+        try {
+            OutputStream ost = new FileOutputStream(file);
+            ObjectOutputStream outputStream = null;
+            try {
+                outputStream = new ObjectOutputStream(ost);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            outputStream.writeObject(products);
+            ost.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
